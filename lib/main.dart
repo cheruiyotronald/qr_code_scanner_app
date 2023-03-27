c816aa25e0cadd90c18adb19d66e40ad60767763
Ronald <cheruiyotronald1168@gmail.com>
2023-03-27 14:41:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "QR Code Scanner",
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: QRCodeWidget(),
+    );
+  }
+}
+
+class QRCodeWidget extends StatefulWidget {
+  const QRCodeWidget({super.key});
+
+  @override
+  State<QRCodeWidget> createState() => _QRCodeWidgetState();
+}
+
+class _QRCodeWidgetState extends State<QRCodeWidget> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController? controller;
+  String result = "";
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData.code!;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Code Scanner"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text("Scan Result: $result",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+             ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              ElevatedButton(
+                onPressed: (){
+                  if(result.isNotEmpty){
+                    Clipboard.setData(ClipboardData(text: result));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Copied to Clipboard"),
+                      ),
+                    );
+                  }
+                },
+                child: Text("Copy"),
+               ),
+               ElevatedButton(
+                onPressed: ()async { 
+                  if(result.isNotEmpty){
+                    final Uri _url = Uri.parse(result);
+                    await launchUrl(_url);
+                  }
+                },
+                child: Text("Open"),
+               ),
+            ],
+           ),
+          )
+        ],
+      ),
+    );
+  }
+}
